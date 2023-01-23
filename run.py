5adc885e9b108cac3950d4e2589d81c89fe9a739
Scanu46 <nykb24@gmail.com>
2023-01-23 11:04:46 +0000

    classes

--- run.py
+++ run.py
@@ -2,4 +2,22 @@ from tkinter import *
 import random 
 
 class Sn:
+    pass
+
+class obj:
+    pass
+
+def turn():
+    pass
+
+def direct():
+    pass
+
+def colis():
+    pass
+
+def game_over():
+    pass
+
+
     
