ef42e0c51dca388c3f6f4c8a2aa67103511e1fad
Scanu46 <nykb24@gmail.com>
2023-01-23 11:46:26 +0000

    game_over

--- run.py
+++ run.py
@@ -113,7 +113,10 @@ def colis():
 
 
 def game_over():
-    pass
+
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
+                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
 
 window = Tk()
 window.title("Snake")
