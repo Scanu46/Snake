3db9d9abc104246bfb9426b6334e413fcc88733c
Scanu46 <nykb24@gmail.com>
2023-01-23 11:15:19 +0000

    constants

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from tkinter import *
 import random 
 
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 60
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#B9QEQA"
+FOOD_COLOR = "#008000"
+BACKGROUND_COLOR = "2D2364"
+
 class Sn:
     pass
 
