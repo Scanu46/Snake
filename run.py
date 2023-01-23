16bbef6c2a82ad9eec621647581dca33959e4070
Scanu46 <nykb24@gmail.com>
2023-01-23 11:31:52 +0000

    direct

--- run.py
+++ run.py
@@ -53,10 +53,10 @@ y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-window.bind('<Left>', lambda event: change_direction('left'))
-window.bind('<Right>', lambda event: change_direction('right'))
-window.bind('<Up>', lambda event: change_direction('up'))
-window.bind('<Down>', lambda event: change_direction('down'))
+window.bind('<Left>', lambda event: direct('left'))
+window.bind('<Right>', lambda event: direct('right'))
+window.bind('<Up>', lambda event: direct('up'))
+window.bind('<Down>', lambda event: direct('down'))
 
 
 
